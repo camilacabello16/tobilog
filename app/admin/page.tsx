4695f728f934514e6c97b93fa6c7e3c6faf5fb98
hieubd <buiduchieuuet16@gmail.com>
2024-05-18 14:41:32 +0700

    change type to deploy

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,9 +2,7 @@
 
 import NavigationAdmin from "../components/navigationAdmin";
 
-export default function AdminPage({ children }: Readonly<{
-    children: React.ReactNode;
-}>) {
+export default function AdminPage({ children }: any) {
     return (
         <div className="flex h-full">
             <NavigationAdmin />
